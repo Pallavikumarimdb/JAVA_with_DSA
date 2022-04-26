085c66ee04b85880d0cb24b0102f32ece80755ed
Pallavi <pallavilpmt1995@gmail.com>
2022-04-26 21:42:56 +0530

    Added Recursion Level 1 question

---
+++ src/com/Recursion/Level_1/PrintReverseNo.java
@@ -0,0 +1,18 @@
+package com.Recursion.Level_1;
+
+public class PrintReverseNo {
+
+    public static void main(String[] args) {
+        OneToN(5);
+    }
+
+    public static void OneToN(int n){
+
+        if(n==0){
+            return;
+        }
+        OneToN(n-1);
+        System.out.println(n);
+    }
+
+}
