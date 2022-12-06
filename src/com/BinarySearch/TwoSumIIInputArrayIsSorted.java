7e7a914b75668bdd54e18bae3415db8c42851794
Pallavikumarimdb <pallavilpmt1995@gmail.com>
2022-12-06 21:32:45 +0530

    Two Sum II - Input Array Is Sorted

---
+++ src/com/BinarySearch/TwoSumIIInputArrayIsSorted.java
@@ -0,0 +1,36 @@
+package com.BinarySearch;
+public class TwoSumIIInputArrayIsSorted{
+    public static void main(String[] args) {
+        int a[]={1,2,2,5};
+        int target=4;
+        int[] arr=twoSum(a,target);
+        System.out.println("["+arr[0]+" "+arr[1]+"]");
+
+        
+    }
+    public static int[] twoSum(int[] number, int target) {
+
+        int start=0;
+        int end=number.length-1;
+        int[] arr=new int[2];
+        while(start<end){
+
+            int mid=start+(end-start)/2;
+            if(number[start]+number[end]==target){            
+                arr[0]=start+1;
+                arr[1]=end+1;
+                return arr;
+            }
+           
+            else if(number[start]+number[end]>target){
+                end=end-1;
+            }else if(number[start]+number[end]<target){
+                start=start+1;
+            }
+        
+
+        }
+        return arr;
+    }
+
+}
