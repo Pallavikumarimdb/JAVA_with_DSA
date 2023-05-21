deba72ea2a6bf9a4bc8aeb6000cd59bb7b41fa17
Pallavi <pallavilpmt1995@gmail.com>
2023-05-21 22:26:34 +0530

    Pretty Printing

--- src/com/Recursion/Intro/FibonacciNumber.java
+++ src/com/Recursion/Intro/FibonacciNumber.java
@@ -1,5 +1,15 @@
 package com.Recursion.Intro;
 
+
+// step 1:
+// when you write recursio in terms of formula or when we write a problem as experssion is called reccurence relation
+// febo(n) = febo(n-1) + febo(n-2) // this is known as reccurence relation
+
+// step 2:
+// Fegure out recursive tree
+// Break it down into smaller problem
+
+
 public class FibonacciNumber {
 
     public static void main(String[] args) {
@@ -8,6 +18,10 @@ public class FibonacciNumber {
 
     public static int fibonacci(int n){
 
+        // Base condition
+//        if(n<2){
+//            return n;
+//        }
         if(n==0 || n==1){
             return n;
         }
