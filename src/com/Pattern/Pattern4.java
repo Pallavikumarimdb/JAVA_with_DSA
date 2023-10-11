b74c797f38091809eeccc9af1bbad330127f5724
Pallavi <pallavilpmt1995@gmail.com>
2023-10-11 14:05:01 +0530

    Pattern question 5

---
+++ src/com/Pattern/Pattern4.java
@@ -0,0 +1,24 @@
+package com.Pattern;
+
+// QUESTION
+//1
+//1 2
+//1 2 3
+//1 2 3 4
+//1 2 3 4 5
+
+public class Pattern4 {
+    public static void main(String[] args) {
+        pattern(5);
+    }
+    static void pattern(int n){
+        for (int row = 1; row <=n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col + " ");
+
+            }
+            System.out.println();
+        }
+    }
+
+}
---
+++ src/com/Pattern/Pattern5.java
@@ -0,0 +1,42 @@
+package com.Pattern;
+
+// QUESTIONS
+//*
+//**
+//***
+//****
+//*****
+//****
+//***
+//**
+//*
+
+public class Pattern5 {
+    public static void main(String[] args) {
+        pattern(5);
+    }
+    static void pattern(int n){
+        for (int row = 1; row <2*n; row++) {
+            int c;
+//      row   col
+//*      1     1
+//**     2     2
+//***    3     3
+//****   4 eql 4     // till here we can do the same as Pattern1 question but after this step the method is different hence we need to write if else condition
+//***    5     3     // From here the value of row is greater than n and here onward we are reducing one "*" at every step compare to above.
+//**     6     2     // Hence we need formula to print "*" at every place. After calculating formula will be "n-[row-n] or 2n-row]".
+//*      7     1
+
+            if(row>n){
+                c = 2*n-row;
+            }else {
+                c=row;
+            }
+            for (int col = 1; col <=c; col++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+}
