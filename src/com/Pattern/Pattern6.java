c6623949b4f9129b1fc93652426a710c7b2234c7
Pallavi <pallavilpmt1995@gmail.com>
2023-10-12 20:28:52 +0530

    Pattern question 6

---
+++ src/com/Pattern/Pattern6.java
@@ -0,0 +1,27 @@
+package com.Pattern;
+
+// QUESTION
+//        *
+//        **
+//        ***
+//        ****
+//        *****
+public class Pattern6 {
+    public static void main(String[] args) {
+        pattern(5);
+    }
+
+    static void pattern(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int sp = 0; sp < n-row; sp++) {
+                System.out.print(" ");
+            }
+            // for every row, run the col
+            for (int col = 1; col <= row; col++) {
+                System.out.print("*");
+            }
+            // When one row is printed, we need to add a newline
+            System.out.println();
+        }
+    }
+}
