f19787ba65b90f60883beeeff98517753528370f
Pallavi <pallavilpmt1995@gmail.com>
2024-08-15 21:24:19 +0530

    Notes

---
+++ src/com/_4_First_JAVA_Program/Main.java
@@ -0,0 +1,9 @@
+package com._4_First_JAVA_Program;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextLine());
+    }
+}
---
+++ src/com/_4_First_JAVA_Program/notes.md
@@ -0,0 +1,1 @@
+<h1 align="center"> *FIRST JAVA PROGRAM* </h1>
