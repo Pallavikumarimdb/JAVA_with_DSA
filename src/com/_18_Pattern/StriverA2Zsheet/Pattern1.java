ce61b60abea0c9fc2dd8cf509d8343e2352c0237
Pallavi <pallavilpmt1995@gmail.com>
2024-08-23 22:13:48 +0530

    Pattern Striver A2Z sheet

---
+++ src/com/_18_Pattern/StriverA2Zsheet/Pattern1.java
@@ -0,0 +1,25 @@
+package com._18_Pattern.StriverA2Zsheet;
+
+import java.util.Scanner;
+
+//*****
+//*****
+//*****
+//*****
+//*****
+/* No of row =No of Column
+in each row we need to print n *
+Hence outer and inner loop will run same no of times i.e n.
+ */
+public class Pattern1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
