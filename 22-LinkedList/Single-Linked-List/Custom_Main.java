d5e270eff5166c879d0c084764b5d127cfa59ff6
Pallavi <pallavilpmt1995@gmail.com>
2024-11-19 14:01:00 +0530

    prime no

--- 22-LinkedList/Single-Linked-List/Custom_Main.java
+++ 22-LinkedList/Single-Linked-List/Custom_Main.java
@@ -1,5 +1,24 @@
 public class Custom_Main {
 
 
+    // Here Custom LinkedList will be initialised with size 0;
 
+    public static void main(String[] args) {
+        Custom_LinkedList list=new Custom_LinkedList();
+
+        list.insertFirst(3);
+        list.insertFirst(4);
+        list.insertFirst(8);
+        list.insertFirst(24);
+        list.insertFirst(48);
+        list.insertLast(100);
+        list.insert(78 ,4);
+        list.display();
+        System.out.println(list.deleteFirst());
+        list.display();
+        System.out.println(list.deleteLast());
+        list.display();
+        System.out.println(list.delete(2));
+        list.display();
+    }
 }
